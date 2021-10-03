f84e80ae7a7c7a213c3565ed7a4a4f1835752a74
Leonid Shustov <leonshustov1995@gmail.com>
2021-10-03 22:17:20 +0300

    refactoring & add EventOrOdd

--- Codewars/6kyu/MorseCode/MorseCode.js
+++ Codewars/6kyu/MorseCode/MorseCode.js
@@ -65,18 +65,17 @@ decodeMorse = function(morseCode){
     let transWordsArr = []
 
 
-    for(let i = 0; i < morseCodeWordsArr.length; i++){
+    for(let i = 0; i < morseCodeWordsArr.length; i++) {
         //Get one word
         let word = morseCodeWordsArr[i].split(" ")
 
-        for(let j = 0; j < word.length; j++){          // go through the symbols and push the value
-            if(Object.keys(morse).includes(word[j])){
+        for (let j = 0; j < word.length; j++) {          // go through the symbols and push the value
+            if (Object.keys(morse).includes(word[j])) {
                 transWordsArr.push(morse[word[j]])
             }
         }
         transWordsArr.push(' ')      // add spase after words
     }
-
 
     return transWordsArr.join('').trim()
 
