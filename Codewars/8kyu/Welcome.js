1408eed5d71de12c158fea43d9ce67355778f287
Leonid Shustov <leonshustov1995@gmail.com>
2021-10-07 19:50:28 +0300

    add Welcome

---
+++ Codewars/8kyu/Welcome.js
@@ -0,0 +1,26 @@
+function greet(language) {
+    const languages = {
+        english: 'Welcome',
+        czech: 'Vitejte',
+        danish: 'Velkomst',
+        dutch: 'Welkom',
+        estonian: 'Tere tulemast',
+        finnish: 'Tervetuloa',
+        flemish: 'Welgekomen',
+        french: 'Bienvenue',
+        german: 'Willkommen',
+        irish: 'Failte',
+        italian: 'Benvenuto',
+        latvian: 'Gaidits',
+        lithuanian: 'Laukiamas',
+        polish: 'Witamy',
+        spanish: 'Bienvenido',
+        swedish: 'Valkommen',
+        welsh: 'Croeso'
+    }
+
+    if(Object.keys(languages).includes(language)){
+        return languages[language]
+    }
+    return languages['english']
+}
